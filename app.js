60726982acf919868267d71e7819910d0b536bf3
5738524662663764 <48597880+5738524662663764@users.noreply.github.com>
2019-03-16 22:02:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/HeHeBOI/',
+    prefix: '/proxy/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
